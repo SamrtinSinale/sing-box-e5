d00aa50f00370ea9bd3197ecf87bedd0f8884fbc
CHIZI-0618 <kay9925@outlook.com>
2025-02-04 23:47:05 +0800

    Add a bad android process searcher
    Reference from https://github.com/Joeingarm/mihomo/commit/a068ca150c7ef4844766de60b38a36d5536b9495

--- common/process/searcher_android.go
+++ common/process/searcher_android.go
@@ -5,6 +5,10 @@ import (
 	"net/netip"
 
 	"github.com/sagernet/sing-tun"
+	E "github.com/sagernet/sing/common/exceptions"
+
+	"github.com/shirou/gopsutil/v4/net"
+	"github.com/shirou/gopsutil/v4/process"
 )
 
 var _ Searcher = (*androidSearcher)(nil)
@@ -18,7 +22,7 @@ func NewSearcher(config Config) (Searcher, error) {
 }
 
 func (s *androidSearcher) FindProcessInfo(ctx context.Context, network string, source netip.AddrPort, destination netip.AddrPort) (*Info, error) {
-	_, uid, err := resolveSocketByNetlink(network, source, destination)
+	uid, err := resolveSocketUID(source.Addr(), source.Port())
 	if err != nil {
 		return nil, err
 	}
@@ -34,5 +38,55 @@ func (s *androidSearcher) FindProcessInfo(ctx context.Context, network string, s
 			PackageName: packageName,
 		}, nil
 	}
-	return &Info{UserId: int32(uid)}, nil
+
+	name, path, err := resolveProcessNamePathByUID(uid)
+	if err != nil {
+		return nil, err
+	}
+	info := Info{
+		UserId: int32(uid),
+	}
+	if name != "" {
+		info.PackageName = name
+	}
+	if path != "" {
+		info.ProcessPath = path
+	}
+	return &info, nil
+}
+
+func resolveSocketUID(srcIP netip.Addr, srcPort uint16) (uint32, error) {
+	connections, err := net.Connections("all")
+	if err != nil {
+		return 0, err
+	}
+
+	for _, conn := range connections {
+		if conn.Laddr.Port == uint32(srcPort) && conn.Laddr.IP == srcIP.String() {
+			if len(conn.Uids) > 0 {
+				return uint32(conn.Uids[0]), nil
+			}
+		}
+	}
+	return 0, ErrNotFound
+}
+
+func resolveProcessNamePathByUID(uid uint32) (string, string, error) {
+	processes, err := process.Processes()
+	if err != nil {
+		return "", "", err
+	}
+
+	for _, p := range processes {
+		puid, err := p.Uids()
+		if err == nil && len(puid) > 0 && uint32(puid[0]) == uid {
+			name, err1 := p.Name()
+			path, err2 := p.Exe()
+			if err1 != nil || err2 != nil {
+				continue
+			}
+			return name, path, nil
+		}
+	}
+	return "", "", E.New("process of uid not found", uid)
 }
