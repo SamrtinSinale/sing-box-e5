reF1nd/sing-box

Add TCP keep alive options to outbound provider's `override_dialer`

provider/parser/parser.go
@@ -136,5 +136,11 @@ func overrideDialerOption(options option.DialerOptions, overrideDialerOptions *o
 	if overrideDialerOptions.FallbackDelay != nil {
 		options.FallbackDelay = *overrideDialerOptions.FallbackDelay
 	}
+	if overrideDialerOptions.TCPKeepAliveInterval != nil {
+		options.TCPKeepAliveInterval = *overrideDialerOptions.TCPKeepAliveInterval
+	}
+	if overrideDialerOptions.TCPKeepAliveIdle != nil {
+		options.TCPKeepAliveIdle = *overrideDialerOptions.TCPKeepAliveIdle
+	}
 	return options
 }

SamrtinSinale/sing-box-e5

provider/parser/parser.go
@@ -168,6 +168,12 @@ func overrideDialerOption(options option.DialerOptions, overrideDialerOptions *o
 	if overrideDialerOptions.TCPMultiPath != nil {
 		options.TCPMultiPath = *overrideDialerOptions.TCPMultiPath
 	}
+	if overrideDialerOptions.TCPKeepAlive != nil {
+		options.TCPKeepAlive = *overrideDialerOptions.TCPKeepAlive
+	}
+	if overrideDialerOptions.TCPKeepAliveInterval != nil {
+		options.TCPKeepAliveInterval = *overrideDialerOptions.TCPKeepAliveInterval
+	}
 	if overrideDialerOptions.UDPFragment != nil {
 		options.UDPFragment = overrideDialerOptions.UDPFragment
 	}
@@ -183,6 +189,12 @@ func overrideDialerOption(options option.DialerOptions, overrideDialerOptions *o
 	}
 	if overrideDialerOptions.FallbackDelay != nil {
 		options.FallbackDelay = *overrideDialerOptions.FallbackDelay
+	}
+	if overrideDialerOptions.TCPKeepAliveCount != nil {
+		options.TCPKeepAliveCount = *overrideDialerOptions.TCPKeepAliveCount
+	}
+	if overrideDialerOptions.DisableTCPKeepAlive != nil {
+		options.DisableTCPKeepAlive = *overrideDialerOptions.DisableTCPKeepAlive
 	}
 
 	//nolint:staticcheck